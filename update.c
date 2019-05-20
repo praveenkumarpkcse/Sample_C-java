aa13ac8fd06cdfd821ad3a9c486c3a2cee139e4e
praveenkumarpkcse <50762992+praveenkumarpkcse@users.noreply.github.com>
2019-05-20 16:17:32 +0530

    Create update.c

---
+++ update.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+   int n;
+int a[100],temp;
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+
+for(int j=0;j<n;j++)
+{
+    for(int k=j+1;k<n;k++)
+    {
+        if(a[j]>a[k])
+        {
+            a[j]=a[j]+a[k];
+            a[k]=a[j]-a[k];
+            a[j]=a[j]-a[k];
+        }
+    }
+}
+for(int i=0;i<n;i++)
+{
+    printf("%d ",a[i]);
+}
+}
